43d44e750b810dc59e5de5d27aca5c3427cd1948
Al Ev <alexey_e@mail.ru>
2024-11-27 20:21:57 +0300

    making promise

--- L-3/run.js
+++ L-3/run.js
@@ -1,4 +1,12 @@
 // Напишите программу в которой каждые 3 секунды в вечном цикле будет выводиться
 // текущая дата и случайная фраза из массива с пожеланиями пользователю
 
-console.log('git');
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+(async () => {
+    while (true){
+        await sleep(3000)
+        console.log('Hello User!');
+    }
+})

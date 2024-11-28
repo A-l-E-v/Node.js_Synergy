6e76dfa9b61537697b9513e0fa8cc08898bfedc1
Al Ev <alexey_e@mail.ru>
2024-11-28 10:21:44 +0300

    L-3 done!

--- L-3/run.js
+++ L-3/run.js
@@ -1,12 +1,29 @@
 // Напишите программу в которой каждые 3 секунды в вечном цикле будет выводиться
 // текущая дата и случайная фраза из массива с пожеланиями пользователю
 
+const Greetings = ['Hello', 'Hola', 'Bonjour', 'Hallo']
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const showGreeting = () => Greetings[Math.floor(Math.random() * Greetings.length)]
+
+
+
+const showDate = () => {
+    let today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = dd + '.' + mm + '.' + yyyy;
+    console.log(today, showGreeting());
+}
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
 
 (async () => {
-    while (true){
-        await sleep(3000)
-        console.log('Hello User!');
+    while (true) {
+        showDate()
+        await sleep(3000);
     }
-})
+})();
+
